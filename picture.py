d22829bfe5e603f4e42cccecf8ac651bca9ef156
Sean_2525 <madness48596@gmail.com>
2016-09-29 17:54:19 +0800

    change

--- picture.py
+++ picture.py
@@ -3,17 +3,14 @@ from bs4 import BeautifulSoup
 s=requests.session()
 def request(web):
     r=s.get(url=web)                                        #Get url
-    r = BeautifulSoup(r.text,"lxml")                        #use bs4 and lxml 解析網頁
+    r=BeautifulSoup(r.text,"lxml")                        #use bs4 and lxml 解析網頁
     grab_picture(r)
 def grab_picture(self):
     a=1                                                     # set filename number
     for picture in self.select('img[class="BDE_Image"]'):
         r=requests.get(picture['src'])                      #get the image url
         pt = '/home/mino/Pictures/test/{0}.jpg'.format(a)   #filename
-        with open(pt,'wb') as f:                            #save jpg
-        r=requests.get(picture['src'])
-        filename = '/home/mino/Pictures/test/{0}.jpg'.format(a)
-        with open(filename,'wb') as f:
+        with open(pt,'wb') as f:
             f.write(r.content)
         a=a+1
 if __name__ == "__main__":
